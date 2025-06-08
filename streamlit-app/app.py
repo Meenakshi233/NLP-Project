bc80e43523d2ca5e35210bbb80c41bf4f1ea5e12
Meenakshi233 <153627608+Meenakshi233@users.noreply.github.com>
2025-06-08 19:56:47 +0530

    Update app.py

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -3,6 +3,7 @@ import helper
 import pickle
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "model.pkl")
 model = pickle.load(open(model_path, 'rb'))
 
